ca09cfdbe03e0d7eab05ffe97ab83a5770a51444
Carlos Cortés Sánchez <ccortes@gmv.com>
2017-12-21 14:35:47 +0100

    Add transition coordinator

---
+++ Pong/TransitionCoordinator.swift
@@ -0,0 +1,13 @@
+//
+//  TransitionCoordinator.swift
+//  Pong
+//
+//  Created by Carlos Cortés Sánchez on 21/12/2017.
+//  Copyright © 2017 Luke Parham. All rights reserved.
+//
+
+import UIKit
+
+class TransitionCoordinator: NSObject, UINavigationControllerDelegate {
+    
+}
